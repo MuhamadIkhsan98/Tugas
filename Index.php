ac94997addf0f0f008ab418d28eeb6e5057b3b4b
MuhamadIkhsan98 <48472852+MuhamadIkhsan98@users.noreply.github.com>
2019-03-12 16:01:13 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "HELLO WORD !";
+echo "<br>";
+echo "Nama : M.Ikhsan";
+echo "<br>";
+echo "Nim : 16.01.53.0112";
+echo "<br>";
+?> 
+
+</body>
+</html>
